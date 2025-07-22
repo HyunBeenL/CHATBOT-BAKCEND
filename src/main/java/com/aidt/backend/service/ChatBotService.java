cea22299681c25598bcc71c3bd5e5f99345826cf
leehv1234 <leehv1234@chunjae.co.kr>
2025-07-22 17:10:41 +0900

    11

---
+++ src/main/java/com/aidt/backend/service/ChatBotService.java
@@ -0,0 +1,16 @@
+package com.aidt.backend.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
+
+
+@Service
+public class ChatBotService {
+
+
+
+}
